0c623914162d28f15072826e3ef9528682b7c61d
Mihir Bhaskar <mihir44@hotmail.com>
2022-11-16 00:59:41 -0500

    initial code for creating sample for labelling

--- sample_for_labeling.py
+++ sample_for_labeling.py
@@ -5,7 +5,7 @@ import yaml
 from reshape_building_data import reshape_building_data
 
 # Connect to database using database YAML file with connection params
-with open('../database.yaml', 'r') as f:
+with open('database.yaml', 'r') as f:
     db_params = yaml.safe_load(f)
 
 engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{dbname}'.format(
@@ -19,5 +19,19 @@ engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{dbname}'.f
 # Read in buildinginfo
 bi = pd.read_sql('SELECT * from processed.building_info', engine)
 
-# Make sure I drop cases where storyht in buildinginfo sets off a cleaning flag (either = 0, or sqft_flr2 > 0 and storyht <= 1)
+# Keep only buildings where yearbuild is not null and yearbuild < 1930
+bi_pre1930 = bi[(bi['year_built'] < 1930) & (bi['year_built'].isna() == False)]
+
+# Drop buildings that had issues with story height
+bi_pre1930 = bi_pre1930[bi_pre1930['story_ht'] != 0]
+
+# Creating building IDs at the parcel level, that can be used in the reshape function
+bi_pre1930['building_id'] = bi_pre1930.groupby('parcelid').cumcount() + 1
+
+# Make sure I drop cases where storyht in buildinginfo sets off a cleaning flag (either = 0, or sqft_flr2 > 0 and storyht <= 1), or num_stories = 0
 # Drop cases wher mt.prop_class_code != hs.use_code (in historic sales)
+
+# Keep where yearbuild < 1930 and yearbuild is not null
+
+# Create features of yearbuild (decade) and 
+
