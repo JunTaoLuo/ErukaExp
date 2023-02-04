2ed21899f275a99ab56eaa66f65b27d70271ab91
Mihir Bhaskar <mihir44@hotmail.com>
2023-02-04 16:58:54 -0500

    feature additions and changes

--- modeling_pipeline/prep_data.py
+++ modeling_pipeline/prep_data.py
@@ -28,6 +28,7 @@ def read_features(engine):
             SELECT parcelid,
                     bi.attic_sqft, bi.bsmt_sqft, bi.live_sqft, bi.sqft_flr1, 
                     bi.sqft_flr2, bi.sqft_flrh, bi.story_ht, bi.year_built, bi.prop_class_code, bi.class_description,
+                    bi.live_sqft_prop
 
                     hs.number_of_parcels, hs.date_of_sale, hs.appraisal_area, hs.total_sales_records, hs.sale_price,
                     hs.style, hs.grade, hs.exterior_wall_type, hs.basement, hs.heating, hs.air_conditioning, hs.total_rooms,
@@ -36,8 +37,14 @@ def read_features(engine):
 
             FROM processed.building_info bi JOIN processed.historic_sales hs USING(parcelid)
          '''
+         
+    feats = pd.read_sql(sql, engine)
     
-    return pd.read_sql(sql, engine)
+    # Dropping columns we don't want to use for analysis
+    feats = feats.drop(['live_sqft', 'class_description', 'date_of_sale', 'total_sales_records',
+                        'sale_price'], axis=1)
+    
+    return feats
                 
 
 def read_data(engine, keep):
@@ -51,8 +58,11 @@ def read_data(engine, keep):
     
     if merged['prop_class_code'].isna().sum() > 0:
         warnings.warn(f"There is missing feature data for labels where there shouldn't be.")
-
+        
     return merged
+
+# def sample_data(df, n):
+#     return df.sample(n=n, random_state=4)
 
 def split_data(df, test_size = 0.2, random_state = 4):
     y = df['building_value']
@@ -62,18 +72,30 @@ def split_data(df, test_size = 0.2, random_state = 4):
 
     return X_train, X_test, y_train, y_test
 
+def impute_features(X_df):
+    
+    X_df['number_of_parcels'] = X_df['number_of_parcels'].fillna(1) # mode number of parcels
+    
+    return X_df
+    
+
 def process_features(X_df):
-        
-    # Todo: convert categorical variables to one-hot, after doing some clubbing
-    # style, grade, exterior wall type, basement, heating, aircon, garage type
+    
+    # Imputing features as defined in function above
+    X_df = impute_features(X_df)
+    
+    # Converting categorical features into dummies, adding an extra category for NA
+    X_df = pd.get_dummies(X_df, columns=['prop_class_code', 'style', 'appraisal_area',
+                                         'exterior_wall_type', 'heating', 'air_conditioning'], dummy_na = True)
+    
 
     # keep only numeric features so we don't run into problems with different classes (e.g. linear regression),
     # but can relax this later
     X_df = X_df.select_dtypes(['number'])
 
-    # use a simple imputation to address missing values (do something more sophisticated later)
-    imp = SimpleImputer(strategy='mean')
-    X_df = imp.fit_transform(X_df)
+    # # use a simple imputation to address missing values (do something more sophisticated later)
+    # imp = SimpleImputer(strategy='mean')
+    # X_df = imp.fit_transform(X_df)
 
     # use a standard scaler so we don't run into issues with regularization
     scaler = StandardScaler().fit(X_df)
@@ -81,3 +103,5 @@ def process_features(X_df):
 
     return X_df
     
+## TODO on pipeline:
+# End outcome of running this should be to write the outputs to matrices folder
--- modeling_pipeline/run.py
+++ modeling_pipeline/run.py
@@ -17,13 +17,13 @@ import matplotlib.pyplot as plt
 
 import prep_data as utils
 
-def gen_matrices(engine, keep, test_size):
-    df = utils.read_data(engine, keep)
-    X_train, X_test, y_train, y_test = utils.split_data(df, test_size)
-    X_train = utils.process_features(X_train)
-    X_test = utils.process_features(X_test)
+# def gen_matrices(engine, keep, test_size):
+#     df = utils.read_data(engine, keep, n)
+#     X_train, X_test, y_train, y_test = utils.split_data(df, test_size)
+#     X_train = utils.process_features(X_train)
+#     X_test = utils.process_features(X_test)
     
-    return X_train, X_test, y_train, y_test
+#     return X_train, X_test, y_train, y_test
 
 def train(X_train, y_train):
     reg = LinearRegression()
@@ -33,36 +33,36 @@ def train(X_train, y_train):
 def predict(reg, X_test):
     return reg.predict(X_test)
     
-def run_experiment(engine, keep, test_size):
+def run_experiment(n, X_train, X_test, y_train, y_test):
     
     wandb.init(project='eruka-housing', entity='gormleylab',
-               name='testrun_regression',
-               config={'modeltypes': 'linear regression'})
-    
-    X_train, X_test, y_train, y_test = gen_matrices(engine, keep, test_size)
-    
+               name=f'testrun_reg_{n}',
+               config={'modeltype': 'linear regression',
+                       'n': n})
+        
     model = train(X_train, y_train)
     
     y_pred = predict(model, X_test)
     
     rmse = mean_squared_error(y_test, y_pred, squared=False)
-    r2 = r2_score(y_test, y_pred, )
+    r2 = r2_score(y_test, y_pred)
     
     wandb.log({'rmse': rmse, 'r2': r2})
     
-    wandb.sklearn.plot_residuals(model, X_train, y_train)
-    
-    # wandb.sklearn.plot_regressor(reg, X_train, X_test, y_train, y_test, model_name='Ridge')
-    
+   # wandb.sklearn.plot_residuals(model, X_train, y_train)
+        
     wandb.finish()
     
 
 if __name__ == '__main__':
     
+    # Add argparse option to regen matrices (basically whether to run prep_data or no)
+    # If no, just put stuff in the matrices folder.
+    
     # parser = argparse.ArgumentParser(description="Script to run modeling pipeline")
-    # parser.add_argument('-n', '--numsamples', action='store_true', help='Number of labeled points to user from the data')
-    # parser.add_argument('-t', '--types', action='store_true', choices=['simple', 'all'], default='simple',
-    #                                                         help='Whether to keep simple case where no year and not handwritte, or all')
+    # parser.add_argument('n', type=int, help='Number of labeled points to user from the data')
+    # # parser.add_argument('-t', '--types', action='store_true', choices=['simple', 'all'], default='simple',
+    # #                                                         help='Whether to keep simple case where no year and not handwritte, or all')
     # args = parser.parse_args()
 
     # Set DB connection from environment
@@ -73,6 +73,18 @@ if __name__ == '__main__':
     db_uri = os.environ['ERUKA_DB']
     db_engine = create_engine(db_uri)
     
-    run_experiment(db_engine, keep='simple', test_size=0.2)
+    # Get parameter for sample size
+    # n = args.n
     
+    # Read full data
+    df = utils.read_data(db_engine, keep='simple')
+    
+    # Sample increasing amounts
+    n_list = [1000, 2000, 3000, 4000, 5000]
+    for n in n_list:
+        df_s = utils.sample_data(df, n)
+        X_train, X_test, y_train, y_test = utils.split_data(df_s, test_size=0.2)
+        X_train = utils.process_features(X_train)
+        X_test = utils.process_features(X_test)
+        run_experiment(n, X_train, X_test, y_train, y_test)    
     
