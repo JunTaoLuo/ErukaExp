e23f3da0597fec0c3f9f74a3a9d759534ca6d097
Mihir Bhaskar <mihir44@hotmail.com>
2023-02-19 16:15:14 -0500

    adding some logging, running with more data

--- modeling_pipeline/prep_data.py
+++ modeling_pipeline/prep_data.py
@@ -2,14 +2,16 @@
 prep_data.py
 Author: Mihir Bhaskar
 Purpose: pipeline for creating the train-test matrices for modeling
+
+Notes: for now, I log using print statements to command line because the pipeline
+is very simple and local. If it gets more compliated, will start persisting and printing
+to a log file.
 '''
 import pandas as pd
 import numpy as np
 import warnings
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
-#TODO: add logging statements
 
 def read_labels(engine, keep):
     '''
@@ -29,7 +31,11 @@ def read_labels(engine, keep):
                  FROM samples.building_values 
                  WHERE year is null and handwritten is null and building_value is not null'''
 
-    return pd.read_sql(sql, engine)
+    labels = pd.read_sql(sql, engine)
+    
+    print(f"Read labels: there are {len(labels)} observations")
+
+    return labels
 
 def read_features(engine):
     '''
@@ -50,14 +56,14 @@ def read_features(engine):
                     bi.attic_sqft, bi.bsmt_sqft, bi.live_sqft, bi.sqft_flr1, 
                     bi.sqft_flr2, bi.sqft_flrh, bi.story_ht, bi.year_built, bi.prop_class_code, bi.class_description,
                     bi.live_sqft_prop,
-
                     hs.number_of_parcels, hs.date_of_sale, hs.appraisal_area, hs.total_sales_records, hs.sale_price,
                     hs.style, hs.grade, hs.exterior_wall_type, hs.basement, hs.heating, hs.air_conditioning, hs.total_rooms,
                     hs.full_bath, hs.half_bath, hs.fireplaces, hs.garage_type, hs.finished_sq_ft, hs.total_finish_area,
                     hs.first_floor_area, hs.half_floor_area, hs.finished_basement, hs.garage_capacity, 
                     hs.grade_grouped, hs.grade_numeric, hs.basement_grouped, hs.garage_type_grouped
-
             FROM processed.building_info bi JOIN processed.historic_sales hs USING(parcelid)
+            WHERE parcelid IN (SELECT parcelid FROM samples.building_values 
+                                WHERE year is null and handwritten is null and building_value is not null)
          '''
          
     feats = pd.read_sql(sql, engine)
@@ -66,6 +72,8 @@ def read_features(engine):
     feats = feats.drop(['live_sqft', 'class_description', 'date_of_sale', 'total_sales_records',
                         'sale_price', 'grade_grouped', 'grade', 'basement', 'garage_type',
                         'finished_sq_ft', 'total_finish_area', 'first_floor_area'], axis=1)
+    
+    print(f"Finished reading features: {feats.shape[1]} columns were read in")
     
     return feats
                 
@@ -95,6 +103,8 @@ def read_data(engine, keep):
                                          'exterior_wall_type', 'heating', 'air_conditioning', 'basement_grouped',
                                          'garage_type_grouped'], dummy_na = True)
         
+    print(f"Merged labels and features. The data shape is {merged.shape}")
+    
     return merged
 
 def split_data(df, test_size = 0.2, random_state = 4):
@@ -112,6 +122,12 @@ def split_data(df, test_size = 0.2, random_state = 4):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size, random_state = random_state)
 
+    print(f'''Data was split into into X, y, train and test. The shapes are:
+              \nX_train: {X_train.shape}
+              \nX_test: {X_test.shape}
+              \ny_train: {y_train.shape}
+              \ny_test: {y_test.shape}''')
+
     return X_train, X_test, y_train, y_test
 
 def impute_features(X_df):
@@ -128,6 +144,8 @@ def impute_features(X_df):
     # Note: as observed in the feature sheet, because of the cleaning and cohort selection process,
     # there aren't many features with missing values in the relevant data. We can consider things
     # like median/mode imputation
+    
+    print("Features were imputed!")
     
     return X_df
     
@@ -155,6 +173,8 @@ def process_features(X_df):
     # use a standard scaler so we don't run into scale issues with regularization
     scaler = StandardScaler().fit(X_df)
     X_df = scaler.transform(X_df)
+    
+    print("Features were processed!")
 
     return X_df, colnames
 
@@ -170,21 +190,27 @@ def gen_matrices(X_train, X_test, y_train, y_test, colnames, matrix_path='matric
     purpose:
         - arrays are faster than pandas dfs for processing
     '''
-    # TODO: Couldn't find a good way to loop this stuff, but explore - maybe locals?
-    # Put an if-condition (X_dfs are probably already numpy arrays)
-   # X_train = np.to_numpy(X_train)
-   # X_test = np.to_numpy(X_test)
-    y_train = y_train.to_numpy()
-    y_test = y_test.to_numpy()
+    for df in [X_train, X_test, y_train, y_test]:
+        if not isinstance(df, np.ndarray):
+            df = df.to_numpy()
+            
+    # X_train = np.to_numpy(X_train)
+    # X_test = np.to_numpy(X_test)
+    # y_train = y_train.to_numpy()
+    # y_test = y_test.to_numpy()
     
     np.savetxt(f"{matrix_path}/X_train.txt", X_train)
     np.savetxt(f"{matrix_path}/X_test.txt", X_test)
     np.savetxt(f"{matrix_path}/y_train.txt", y_train)
     np.savetxt(f"{matrix_path}/y_test.txt", y_test)
     
+    print("Matrices saved to file")
+    
     # Export colnames
     with open(f"{matrix_path}/colnames.txt", 'w') as file:
         file.write('\n'.join(colnames))
+    
+    print("Column names saved to file")
     
     return X_train, X_test, y_train, y_test
     
