bbaeb091973470576a9a9f65c77aa4a7896b471f
JunTaoLuo <jtluo@andrew.cmu.edu>
2023-03-25 16:51:05 -0400

    Update populate samples for franklin county

--- label_data/constants.py
+++ label_data/constants.py
@@ -16,11 +16,13 @@ ref_csv_file = os.path.join(data_dir, "ref.csv")
 # Parameters
 
 db_params = {
-    "samples_label_table": "samples.labels",
-    "samples_building_values_table": "samples.building_values",
-    "samples_download_time_table": "samples.download_time",
-    "samples_label_time_table": "samples.label_time",
-    "samples_error_table": "samples.error",
+    "franklin_schema": "franklin",
+    "hamilton_schema": "samples",
+    "label_table": "labels",
+    "building_values_table": "building_values",
+    "download_time_table": "download_time",
+    "label_time_table": "label_time",
+    "error_table": "error",
     "building_info_source": "processed.building_info",
     "historic_sales_source": "processed.historic_sales"
 }
